436f5912d74dd10f929b1065f1b22de63d0ff20e
Jaime Deng <jaime@jaimes-mbp.wifi.bcit.ca>
2019-09-17 16:12:32 -0700

    commit

---
+++ test.js
@@ -0,0 +1,27 @@
+const number = 8;
+if (number !== 8) {
+    console.log("Night")
+} else {
+    console.log("Morning")
+}
+
+let num = 8;
+
+function operant(add)
+{
+    console.log(add);
+}
+
+operant(num);
+
+var list = "";
+var i;
+
+var text = [1, 2, 3, 4, 5, 6, 7];
+var i;
+
+for (i = 0; i < 5; i++) //list should go to 5
+{
+    console.log(text[i]);
+}
+
